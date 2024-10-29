3c9bc243a7b6aedef7b45d3c334ca280b170387d
21522288 <21522288@gm.uit.edu.vn>
2024-10-29 13:46:27 +0700

    first commit

---
+++ DemoFacade/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using DemoFacade.Models;
+using DemoFacade.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoFacade.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly LocalFileStorageService _fileStorageService = new LocalFileStorageService();
+
+       
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("File không hợp lệ.");
+            }    
+
+
+            var fileUrl = await _fileStorageService.SaveFileAsync(file);
+            ViewBag.FileUrl = $"{Request.Scheme}://{Request.Host}{fileUrl}";
+
+            return View("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Visibility(string url) => View((object)url);
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DemoFacade/Services/LocalFileStorageService.cs
@@ -0,0 +1,23 @@
+﻿namespace DemoFacade.Services
+{
+    public class LocalFileStorageService
+    {
+        private readonly string _storagePath = "wwwroot/uploads";
+
+        public async Task<string> SaveFileAsync(IFormFile file)
+        {
+            if (!Directory.Exists(_storagePath))
+                Directory.CreateDirectory(_storagePath);
+
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+            var filePath = Path.Combine(_storagePath, fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return $"/uploads/{fileName}";
+        }
+    }
+}
